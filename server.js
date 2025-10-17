fdc64ca52cc8b23d0736ed3bce7cd8467bc39565
brianho123 <hobrian71@gmail.com>
2025-10-18 03:32:03 +0800

    Made a simple backend server using Node.js and Express

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const app = express()
+const PORT = 8383
+
+let data = ['Brian']
+
+
+app.use(express.json())
+
+app.get('/', (req,res) => {
+    res.send(`
+        <body
+            style="background:grey;
+            color: yellow;">
+            <h1>DATA:</h1>
+                <p>${JSON.stringify(data[0])}</p>
+                <a href="/dashboard">Dashboard</a>
+        </body>
+        `)
+})
+
+app.get('/dashboard', (req, res) =>
+{
+    res.send(`
+        <body>
+            <h1>dashboard</h1>
+            <a href="/">Home</a>
+        </body>
+        `)
+})
+
+app.get('/api/data', (req,res) => {
+    console.log('This one was for data')
+    res.send(data)
+})
+
+app.post('/api/data', (req,res) =>
+{
+    const newEntry = req.body
+    console.log(newEntry)
+    data.push (newEntry.name)
+    res.sendStatus(201)
+    
+})
+
+app.delete('/api/data', (req,res) => {
+    data.pop()
+    console.log('We deleted the element off the end of the array')
+    res.sendStatus(203)
+})
+
+app.listen(PORT, () => console.log(`Server has started on: ${PORT}`))
+
